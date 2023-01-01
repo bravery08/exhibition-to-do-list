8a29a7d58038fff4b363f49b0b17ecc355e8c778
Brooke <08bravery@gmail.com>
2023-01-01 09:53:00 -0700

    fix checkboxes to individual functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,20 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Exhibition To-Do List';
-  checked = false;
 
   deleteTask(index: number) {
     this.items.splice(index, 1);
   }
 
-  public items: string[] = [];
+  public items: { description: string; checked: boolean }[] = [];
   public newTask!: string;
 
   addItem() {
     if (this.newTask == '') {    
     }
     else {
-      this.items.push(this.newTask);
+      this.items.push({ description: this.newTask, checked: false });
       this.newTask = '';
     }
   }
