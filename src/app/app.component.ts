81a24526d9a4c2966290cfe230fed839790697bb
Brooke <08bravery@gmail.com>
2023-01-01 09:00:18 -0700

    create to-do list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Exhibition To-Do List';
+  checked = false;
+
+  deleteTask(index: number) {
+    this.items.splice(index, 1);
+  }
+
+  public items: string[] = [];
+  public newTask!: string;
+
+  addItem() {
+    if (this.newTask == '') {    
+    }
+    else {
+      this.items.push(this.newTask);
+      this.newTask = '';
+    }
+  }
 }
